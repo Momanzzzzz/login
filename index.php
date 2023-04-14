397a88f8a515afeba180fcdc75b11facb2df2610
Momanzzzzz <128180936+Momanzzzzz@users.noreply.github.com>
2023-04-14 05:36:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class="main">
+    <h1>login</h1>
+    <i>Welcome</i><br><br>
+    <input type= "text" name="username" id="username" placeholder="username"><br>
+    <input type= "password" name="password" id="password"placeholder="password"><br>
+    <input type= "submit" name="submit" id="submit" value="login"><br>
+    <h1>or</h1><br>
+<a id="Register" href="register.php">Register</a> 
+<div>
+</body>
+</html>
